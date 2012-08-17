ada012d74ccc09d8df9e91660ec6f5a15312477a
lonhu0@gmail.com <lonhu0@gmail.com@78cb4c89-a5a2-0b67-5bcc-eee32f0ed1a9>
2012-08-17 23:17:49 +0000

    .

--- trunk/kijiwatcher/src/fetch.py
+++ trunk/kijiwatcher/src/fetch.py
@@ -1,14 +1,12 @@
-import urllib
-import os
+import urllib2
 
-from google.appengine.ext import db
-from google.appengine.api import users
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
-from google.appengine.ext.webapp import template
+from google.appengine.api import urlfetch
+
+
 
 class Fetcher(webapp.RequestHandler):
     def get(self):
-       
-        path = os.path.join(os.path.dirname(__file__), 'index.html')
-        self.response.out.write('Fetcher !!!')
+        url = "http://d1dsp.westjet.com"
+        result = urlfetch.fetch(url)
+        self.response.out.write(result.content)
